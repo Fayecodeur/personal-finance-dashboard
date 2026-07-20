71d33b625fddd3f46f7839b19ede438d62f6d92e
Demba Faye <fayecodeur99@gmail.com>
2026-07-20 20:20:16 +0000

    feat: formulaire register avec Angular Material et snackbar

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
 import { Auth } from '../../../core/services/auth';
 
 @Component({
@@ -16,11 +19,12 @@ import { Auth } from '../../../core/services/auth';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatSnackBarModule,
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {
+export class Login implements OnInit {
   form: FormGroup;
   errorMessage = '';
 
@@ -28,11 +32,22 @@ export class Login {
     private fb: FormBuilder,
     private authService: Auth,
     private router: Router,
+    private snackBar: MatSnackBar,
   ) {
     this.form = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+  }
+
+  ngOnInit(): void {
+    if (history.state.registered) {
+      this.snackBar.open('Compte créé avec succès !', 'Fermer', {
+        duration: 3000,
+        horizontalPosition: 'right',
+        verticalPosition: 'top',
+      });
+    }
   }
 
   onSubmit(): void {
