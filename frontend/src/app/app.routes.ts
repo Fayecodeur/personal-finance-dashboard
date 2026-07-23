6a8c01e39f6c4218e4d4e03f7a7885cd210f3c4a
Demba Faye <fayecodeur99@gmail.com>
2026-07-23 16:28:02 +0000

    Ajout de la page 404

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,44 +1,61 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth-guard';
 import { guestGuard } from './core/guards/guest-guard';
+
 export const routes: Routes = [
   {
     path: '',
     canActivate: [guestGuard],
     loadComponent: () => import('./features/landing/landing').then((m) => m.Landing),
   },
+
   {
     path: 'login',
     loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
   },
+
   {
     path: 'register',
     loadComponent: () => import('./features/auth/register/register').then((m) => m.Register),
   },
+
   {
     path: 'app',
     canActivate: [authGuard],
     loadComponent: () => import('./layout/main-layout/main-layout').then((m) => m.MainLayout),
+
     children: [
       {
         path: 'dashboard',
         loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.Dashboard),
       },
+
       {
         path: 'transactions',
         loadComponent: () =>
           import('./features/transactions/transactions').then((m) => m.Transactions),
       },
+
       {
         path: 'categories',
         loadComponent: () => import('./features/categories/categories').then((m) => m.Categories),
       },
+
       {
         path: 'profile',
         loadComponent: () => import('./features/profile/profile').then((m) => m.Profile),
       },
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
     ],
   },
-  { path: '**', redirectTo: '' },
+
+  {
+    path: '**',
+    loadComponent: () => import('./shared/not-found/not-found').then((m) => m.NotFound),
+  },
 ];
